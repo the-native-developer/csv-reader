9d5eeb641de7e53fd8b6b1d58450af8755a224cb
Mathias Lüer <mathias.luer@gmail.com>
2023-01-21 14:52:03 +0100

    add edge cases to test

--- reader_test.go
+++ reader_test.go
@@ -19,48 +19,52 @@ func TestSetHeaders(t *testing.T) {
 }
 
 func TestRange(t *testing.T) {
-	headers := []string{"Name", "Age", "Address"}
-	csv := csv.NewReader(strings.NewReader("John,30,New York\nJane,25,Chicago"))
+	headers := []string{"Name", "Age", "", "Address"}
+	csv := csv.NewReader(strings.NewReader("John,30,empty-header,\"New York\",no-header\nJane,25,empty-header, Chicago,no-header"))
 	csv.SetHeaders(headers)
 	i := 0
 	for result := range csv.Range() {
 		if result.Error() != nil {
 			t.Errorf("row had an err: %v", result.Error())
 		}
-
+		i++
 		values := result.Row().Values()
 		for _, column := range headers {
+			if column == "" {
+				continue
+			}
+
 			value, ok := values[column]
 			if !ok {
 				t.Errorf("column %q not found", column)
 			}
+
 			if column == headers[0] {
-				if i == 0 && value != "John" {
+				if i == 1 && value != "John" {
 					t.Errorf("expected Name: John, got: %q", value)
 				}
-				if i == 1 && value != "Jane" {
+				if i == 2 && value != "Jane" {
 					t.Errorf("expected Name: Jane, got: %q", value)
 				}
 			}
 
 			if column == headers[1] {
-				if i == 0 && value != "30" {
+				if i == 1 && value != "30" {
 					t.Errorf("expected Age: i30, got: %q", value)
 				}
-				if i == 1 && value != "25" {
+				if i == 2 && value != "25" {
 					t.Errorf("expected Age: 25, got: %q", value)
 				}
 			}
 
 			if column == headers[2] {
-				if i == 0 && value != "New York" {
+				if i == 1 && value != "New York" {
 					t.Errorf("expected Address: New York, got: %q", value)
 				}
-				if i == 1 && value != "Chicago" {
+				if i == 2 && value != "Chicago" {
 					t.Errorf("expected Address: Chicago, got: %q", value)
 				}
 			}
 		}
-		i++
 	}
 }
